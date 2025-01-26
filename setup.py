e774852094d148a71821eb93f936c537dd6c5e2c
Sameer Prajapati <152258991+SameerPrajapati310@users.noreply.github.com>
2025-01-26 11:45:36 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Sameer Prajapat',
+    author_email= 'sameerprajapati336@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
